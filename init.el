51e897acf517636cff65ff3697a52a829337bb64
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2023-03-06 18:29:10 +0300

    Add which-key package
    Signed-off-by: Stanislav Efremov <efremov.stanislav.v@gmail.com>

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rainbow-delimiters doom-modeline command-log-mode commond-log-mode swiper ivy use-package)))
+   '(which-key rainbow-delimiters doom-modeline command-log-mode commond-log-mode swiper ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -84,3 +84,8 @@
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 0.3))
