051498c61435762c4cb5d629306331d82ee09e48
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2023-03-06 16:45:28 +0300

    Add initial version of "init.el"
    Signed-off-by: Stanislav Efremov <efremov.stanislav.v@gmail.com>

---
+++ init.el
@@ -0,0 +1,86 @@
+;; My first init.el after big break. Typed using touch-typing
+
+;;(setq inhibit-startup-message f
+;;      visible-bell t) ; just a comment
+
+;; Minor modes
+(tool-bar-mode -1)
+(menu-bar-mode 0)
+(scroll-bar-mode -1)
+(recentf-mode 1)
+
+(global-display-line-numbers-mode 1)
+(global-auto-revert-mode 1)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(global-set-key "\M-p" "\M-{")
+(global-set-key "\M-n" "\M-}")
+
+(load-theme 'deeper-blue t)
+
+(column-number-mode)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rainbow-delimiters doom-modeline command-log-mode commond-log-mode swiper ivy use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package command-log-mode
+  :ensure t)
+
+(use-package swiper :ensure t)
+
+(use-package ivy
+;; :diminish
+ :bind (("C-s" . swiper)
+        :map ivy-minibuffer-map
+      ("TAB" . ivy-alt-done)	
+      ("C-l" . ivy-alt-done)
+      ("C-j" . ivy-next-line)
+      ("C-k" . ivy-previous-line)
+      :map ivy-switch-buffer-map
+      ("C-k" . ivy-previous-line)
+      ("C-l" . ivy-done)
+      ("C-d" . ivy-switch-buffer-kill)
+      :map ivy-reverse-i-search-map
+      ("C-k" . ivy-previous-line)
+      ("C-d" . ivy-reverse-i-search-kill))
+ :config
+ (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
