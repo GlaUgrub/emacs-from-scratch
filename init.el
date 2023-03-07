cbb7a1ceee460256c2017b893bc2bb3f08bc92de
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2023-03-07 17:23:09 +0300

    Add package helpful
    Signed-off-by: Stanislav Efremov <efremov.stanislav.v@gmail.com>

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ivy-rich counsel which-key rainbow-delimiters doom-modeline command-log-mode commond-log-mode swiper ivy use-package)))
+   '(helpful ivy-rich counsel which-key rainbow-delimiters doom-modeline command-log-mode swiper ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -56,7 +56,9 @@
  )
 
 (use-package command-log-mode
-  :ensure t)
+  :ensure t
+  :config
+  (command-log-mode 1))
 
 (use-package swiper :ensure t)
 
@@ -101,3 +103,14 @@
 	 ("C-x C-f" . counsel-find-file)
 	 :map minibuffer-local-map
 	 ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
