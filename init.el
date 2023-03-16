62cf612046dbf6f98906499d36b4a07c24e5fbf3
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2023-03-16 20:35:05 +0300

    Reorganize startup files
    1) Add config.org
    2) Move most of config from init.el to config.org

--- init.el
+++ init.el
@@ -1,30 +1,5 @@
 ;; My first init.el after big break. Typed using touch-typing
 
-;;(setq inhibit-startup-message f
-;;      visible-bell t) ; just a comment
-
-;; Minor modes
-(tool-bar-mode -1)
-(menu-bar-mode 0)
-(scroll-bar-mode -1)
-(recentf-mode 1)
-
-(global-display-line-numbers-mode 1)
-(global-auto-revert-mode 1)
-
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(global-set-key "\M-p" "\M-{")
-(global-set-key "\M-n" "\M-}")
-
-(load-theme 'deeper-blue t)
-
-(column-number-mode)
-
 ;; Initialize package sources
 (require 'package)
 
@@ -41,108 +16,21 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit cmake-mode counsel-projectile projectile all-the-icons helpful ivy-rich counsel which-key rainbow-delimiters doom-modeline command-log-mode swiper ivy use-package)))
+   '(switch-window magit cmake-mode counsel-projectile projectile all-the-icons helpful ivy-rich counsel which-key rainbow-delimiters doom-modeline command-log-mode swiper ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
-(use-package command-log-mode
-  :ensure t
-  :config
-  (command-log-mode 1))
-
-(use-package swiper :ensure t)
-
-(use-package ivy
-;; :diminish
- :bind (("C-s" . swiper)
-        :map ivy-minibuffer-map
-      ("TAB" . ivy-alt-done)	
-      ("C-l" . ivy-alt-done)
-      ("C-j" . ivy-next-line)
-      ("C-k" . ivy-previous-line)
-      :map ivy-switch-buffer-map
-      ("C-k" . ivy-previous-line)
-      ("C-l" . ivy-done)
-      ("C-d" . ivy-switch-buffer-kill)
-      :map ivy-reverse-i-search-map
-      ("C-k" . ivy-previous-line)
-      ("C-d" . ivy-reverse-i-search-kill))
- :config
- (ivy-mode 1))
-
-(use-package all-the-icons
-  :ensure t)
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :config
-  (setq which-key-idle-delay 1))
-
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
-
-(use-package counsel
-  :ensure t
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history)))
-
-(use-package helpful
-  :ensure t
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-(use-package projectile
-  :ensure t
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "/media/glaugrub/Data/Projects")
-    (setq projectile-project-search-path '("/media/glaugrub/Data/Projects")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(use-package cmake-mode
-  :ensure t)
-
-(use-package magit
-  :ensure t
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-(custom-set-faces
- ;; other faces
  '(magit-diff-added ((((type tty)) (:foreground "green"))))
  '(magit-diff-added-highlight ((((type tty)) (:foreground "LimeGreen"))))
  '(magit-diff-context-highlight ((((type tty)) (:foreground "default"))))
