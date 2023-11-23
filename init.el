3211d98942f2d8a9c449ad351177f70c390a90ef
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2023-11-23 18:37:52 +0300

    Add window configuration to init.el for IDE use style

--- init.el
+++ init.el
@@ -21,6 +21,29 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(display-buffer-alist
+   '(("*gud-.+"
+      (display-buffer-in-side-window)
+      (side . right)
+      (window-width . 0.3)
+      (slot . -1))
+     ("input/output.+"
+      (display-buffer-in-side-window)
+      (side . right)
+      (window-width . 0.3)
+      (slot . 1))
+     ("*compilation*"
+      (display-buffer-in-side-window)
+      (side . right)
+      (window-width . 0.3)
+      (slot . 1))
+     ("*ivy-occur.+"
+      (display-buffer-in-side-window)
+      (side . right)
+      (window-width . 0.3)
+      (slot . 1))
+     (".*"
+      (display-buffer-same-window display-buffer-reuse-mode-window))))
  '(package-selected-packages
    '(doom-themes switch-window magit cmake-mode counsel-projectile projectile all-the-icons helpful ivy-rich counsel which-key rainbow-delimiters doom-modeline command-log-mode swiper ivy use-package))
  '(safe-local-variable-values
